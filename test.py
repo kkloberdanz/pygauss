13b23ab8a7d89190beabe5c5e8956aa8ef2785a1
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-10-31 15:31:38 -0500

    added mean squared error

--- test.py
+++ test.py
@@ -4,4 +4,12 @@ ans = a.dot([4,5,6])
 
 y = gauss.Vec(2 + i for i in range(10))
 x = gauss.Vec(range(10))
-print(gauss.linear.ordinary_least_squares(x, y))
+assert(gauss.estimator.ordinary_least_squares(x, y) == (1.0, 2.0))
+
+a = gauss.Vec([9.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
+b = gauss.Vec([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+
+assert(gauss.error.mean_squared_error(a, b) == 7.3)
+assert(7.3 == (a - b).square().sum() / len(a))
+
+print('OK')
